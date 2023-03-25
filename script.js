f72abdda84d7288000a895bb65eb160e84982327
Hitesh Marwaha <73687191+hitu69@users.noreply.github.com>
2023-03-25 11:28:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+const ropeLengthsStr = document.getElementById('rope-lengths').value;
+const ropeLengthsArr = ropeLengthsStr.split(',').map(Number);
+const n = ropeLengthsArr.length;
+
+let pq = [];
+for (let i = 0; i < n; i++) {
+pq.push(ropeLengthsArr[i]);
+}
+pq.sort(function (a, b) { return a - b; });
+
+let res = 0;
+while (pq.length > 1) {
+let first = pq.shift();
+let second = pq.shift();
+res += first + second;
+pq.push(first + second);
+pq.sort(function (a, b) { return a - b; });
+}
+
+document.getElementById('result').textContent = res;
+}
